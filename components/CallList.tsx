6ee332326b32387980d7cabfa11438a43ddf3888
Valerie✨ <162357196+valeriekamz@users.noreply.github.com>
2024-09-28 13:57:02 +0300

    Fixed unused variables and React Hook dependencies

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -57,7 +57,7 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
           .flatMap((call) => call.recordings);
   
         setRecordings(recordings);
-      } catch (error) {
+      } catch{
         toast({title: 'Try again later'});
       }
     };
